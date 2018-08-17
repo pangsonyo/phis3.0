315b6882bef989e29303fd5879fcd40b794a6e49
pangsonyo <1015614034@qq.com>
2018-08-17 17:58:04 +0800

    Update OrgService.java

--- src/main/java/com/pyhis/orgmanagment/service/OrgService.java
+++ src/main/java/com/pyhis/orgmanagment/service/OrgService.java
@@ -5,7 +5,7 @@ import com.pyhis.orgmanagment.entity.Org;
 //todo 这里是否要重新定义dto  来对应前端  entity对应表数据
 public interface OrgService {
 
-    void create(Org org);
+    Org create(Org org);
 
-    Org getById(String id);
+    Optional<Org> findOrgById(String id);
 }
