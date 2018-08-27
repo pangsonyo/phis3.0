d9181157379ab2da0ef8918de9357d75f511c0f5
pangsonyo <1015614034@qq.com>
2018-08-27 23:56:12 +0800

    Resolving the global Exception and the Interceptor add by pengxy

--- src/main/java/com/pyhis/orgmanagment/config/SystemInterceptor.java
+++ src/main/java/com/pyhis/orgmanagment/config/SystemInterceptor.java
@@ -2,11 +2,14 @@ package com.pyhis.orgmanagment.config;
 
 import com.pyhis.orgmanagment.exception.UserNotLoginExcetption;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
 
 @Log4j2
 public class SystemInterceptor implements HandlerInterceptor {
@@ -15,9 +18,28 @@ public class SystemInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)  {
         log.info("preHandle");
-       if(1==1) {
-           throw new UserNotLoginExcetption();
-       }
+//       if(1==1) {
+//           throw new UserNotLoginExcetption();
+//       }
+        //the method name of the controller method in the request url  JUST FOR TEST
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String methonName = handlerMethod.getMethod().getName();
+        String className = handlerMethod.getBean().getClass().getSimpleName();
+
+        StringBuffer requestParamBuffer = new StringBuffer();
+        //Map paramMap = 
+         request.getParameterMap().forEach((K,V)->{
+               String mapKey = K;
+               String mapValue = "";
+               Object object = V;
+               if(object instanceof String[]){
+                   String[] strs = (String[]) object;
+                   mapValue = Arrays.toString(strs);
+               }
+             requestParamBuffer.append(mapKey).append("=").append(mapValue);
+         });
+
+
         return false;
     }
 
